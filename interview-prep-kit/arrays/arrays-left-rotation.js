f80398e00722842f79156f5203b7ac1f89a8c338
ASun <allanjsun@gmail.com>
2020-01-03 13:58:54 -0500

    easy arrays

---
+++ interview-prep-kit/arrays/arrays-left-rotation.js
@@ -0,0 +1,11 @@
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    var arr = [];
+    for (var i = 0; i < a.length; i++) {
+        arr.push(a[i]);
+    }
+    for (var j = 1; j <= d; j++) {
+        arr.shift(arr.push(arr[0]));
+    }
+    return arr;
+}
